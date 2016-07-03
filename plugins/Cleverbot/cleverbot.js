18c3cf7da11e6928a9845d8354d5c41652701b83
Remco <hello@dougley.com>
2016-07-03 15:13:58 +0200

    Fix all the things

--- plugins/Cleverbot/cleverbot.js
+++ plugins/Cleverbot/cleverbot.js
@@ -4,15 +4,15 @@ exports.commands = [
 
 var cleverbot = require("cleverbot-node");
 talkbot = new cleverbot;
-cleverbot.prepare(function(){});
+cleverbot.prepare(function() {});
 
 exports.talk = {
-	usage : "<message>",
-	description : "Talk directly to the bot",
-	process : function(bot,msg, suffix) {
-			var conv = suffix.split(" ");
-			talkbot.write(conv, function (response) {
+	usage: "<message>",
+	description: "Talk directly to the bot",
+	process: function(bot, msg, suffix) {
+		var conv = suffix.split(" ");
+		talkbot.write(conv, function(response) {
 			bot.sendMessage(msg.channel, response.message)
-			})
+		})
 	}
 }
--- plugins/Images/images.js
+++ plugins/Images/images.js
@@ -11,13 +11,13 @@ exports.image = {
 	usage: "<search query>",
 	description: "gets the top matching image from google",
 	process: function(bot, msg, args) {
-		if(!AuthDetails || !AuthDetails.youtube_api_key || !AuthDetails.google_custom_search){
+		if (!AuthDetails || !AuthDetails.youtube_api_key || !AuthDetails.google_custom_search) {
 			bot.sendMessage(msg.channel, "Image search requires both a YouTube API key and a Google Custom Search key!");
 			return;
 		}
 		//gets us a random result in first 5 pages
 		var page = 1; //we request 10 items
-		request("https://www.googleapis.com/customsearch/v1?key=" + AuthDetails.youtube_api_key + "&cx=" + AuthDetails.google_custom_search + "&q=" + (args.replace(/\s/g, '+')) + "&searchType=image&alt=json&num=10&start="+page, function(err, res, body) {
+		request("https://www.googleapis.com/customsearch/v1?key=" + AuthDetails.youtube_api_key + "&cx=" + AuthDetails.google_custom_search + "&q=" + (args.replace(/\s/g, '+')) + "&searchType=image&alt=json&num=10&start=" + page, function(err, res, body) {
 			var data, error;
 			try {
 				data = JSON.parse(body);
@@ -25,12 +25,11 @@ exports.image = {
 				console.log(error)
 				return;
 			}
-			if(!data){
+			if (!data) {
 				console.log(data);
 				bot.sendMessage(msg.channel, "Error:\n" + JSON.stringify(data));
 				return;
-			}
-			else if (!data.items || data.items.length == 0){
+			} else if (!data.items || data.items.length == 0) {
 				console.log(data);
 				bot.sendMessage(msg.channel, "No result for '" + args + "'");
 				return;
@@ -45,13 +44,13 @@ exports.rimage = {
 	usage: "<search query>",
 	description: "gets a random image matching tags from google",
 	process: function(bot, msg, args) {
-		if(!AuthDetails || !AuthDetails.youtube_api_key || !AuthDetails.google_custom_search){
+		if (!AuthDetails || !AuthDetails.youtube_api_key || !AuthDetails.google_custom_search) {
 			bot.sendMessage(msg.channel, "Image search requires both a YouTube API key and a Google Custom Search key!");
 			return;
 		}
 		//gets us a random result in first 5 pages
 		var page = 1 + Math.floor(Math.random() * 5) * 10; //we request 10 items
-		request("https://www.googleapis.com/customsearch/v1?key=" + AuthDetails.youtube_api_key + "&cx=" + AuthDetails.google_custom_search + "&q=" + (args.replace(/\s/g, '+')) + "&searchType=image&alt=json&num=10&start="+page, function(err, res, body) {
+		request("https://www.googleapis.com/customsearch/v1?key=" + AuthDetails.youtube_api_key + "&cx=" + AuthDetails.google_custom_search + "&q=" + (args.replace(/\s/g, '+')) + "&searchType=image&alt=json&num=10&start=" + page, function(err, res, body) {
 			var data, error;
 			try {
 				data = JSON.parse(body);
@@ -59,12 +58,11 @@ exports.rimage = {
 				console.log(error)
 				return;
 			}
-			if(!data){
+			if (!data) {
 				console.log(data);
 				bot.sendMessage(msg.channel, "Error:\n" + JSON.stringify(data));
 				return;
-			}
-			else if (!data.items || data.items.length == 0){
+			} else if (!data.items || data.items.length == 0) {
 				console.log(data);
 				bot.sendMessage(msg.channel, "No result for '" + args + "'");
 				return;
@@ -76,12 +74,12 @@ exports.rimage = {
 }
 
 exports.ggif = {
-	usage : "<search query>",
-	description : "get random gif matching tags from google",
-	process : function(bot, msg, args) {
+	usage: "<search query>",
+	description: "get random gif matching tags from google",
+	process: function(bot, msg, args) {
 		//gets us a random result in first 5 pages
 		var page = 1 + Math.floor(Math.random() * 5) * 10; //we request 10 items
-		request("https://www.googleapis.com/customsearch/v1?key=" + AuthDetails.youtube_api_key + "&cx=" + AuthDetails.google_custom_search + "&q=" + (args.replace(/\s/g, '+')) + "&searchType=image&alt=json&num=10&start="+page+"&fileType=gif", function(err, res, body) {
+		request("https://www.googleapis.com/customsearch/v1?key=" + AuthDetails.youtube_api_key + "&cx=" + AuthDetails.google_custom_search + "&q=" + (args.replace(/\s/g, '+')) + "&searchType=image&alt=json&num=10&start=" + page + "&fileType=gif", function(err, res, body) {
 			var data, error;
 			try {
 				data = JSON.parse(body);
@@ -89,12 +87,11 @@ exports.ggif = {
 				console.log(error)
 				return;
 			}
-			if(!data){
+			if (!data) {
 				console.log(data);
 				bot.sendMessage(msg.channel, "Error:\n" + JSON.stringify(data));
 				return;
-			}
-			else if (!data.items || data.items.length == 0){
+			} else if (!data.items || data.items.length == 0) {
 				console.log(data);
 				bot.sendMessage(msg.channel, "No result for '" + args + "'");
 				return;
@@ -102,6 +99,6 @@ exports.ggif = {
 			var randResult = data.items[Math.floor(Math.random() * data.items.length)];
 			bot.sendMessage(msg.channel, randResult.title + '\n' + randResult.link);
 		});
-		
+
 	}
 }
