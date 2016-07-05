4339400ae1843c67cc4fe49ec19c994711fd1a3e
sr229 <aguilarralph13@gmail.com>
2016-07-05 19:42:23 +0800

    format me desu

--- plugins/FAQ/faq.js
+++ plugins/FAQ/faq.js
@@ -1,5 +1,5 @@
 exports.commands = [
-	"faq",
+    "faq",
     "faq_addentry",
     "faq_delentry",
 ]
@@ -7,31 +7,30 @@ exports.commands = [
 var sqlite3 = require('sqlite3').verbose();
 var faq = new sqlite3.Database('faq.db');
 exports.faq_addentry = {
-    usage : "<q> <a>",
-    description : "add a FAQ entry",
-    process:function  addEntry (q, a){
-  index = faq.run("SELECT * FROM faq").length
-  faq.run("INSERT INTO faq VALUES ("+index+","+q+","+a+")")
- }
+    usage: "<q> <a>",
+    description: "add a FAQ entry",
+    process: function addEntry(q, a) {
+        index = faq.run("SELECT * FROM faq").length
+        faq.run("INSERT INTO faq VALUES (" + index + "," + q + "," + a + ")")
+    }
 }
 exports.faq_delentry = {
     usage: "<index>",
     description: "delete a FAQ entry",
     process: function removeEntry(index) {
-        faq.run("DELETE FROM faq WHERE index=" + index)
         if (faq.run("DELETE FROM faq WHERE index=" + index)) {
             bot.SendMessage(msg.author + ", FAQ entry deleted.Happy?");
         }
     }
 }
 exports.faq = {
-usage : "<q>",
-description : "when in doubt.",
-// not sure how mart implemented this
-//but I have to make sure bot prints the file.
-process:function getEntry(query){
-  result = faq.run("SELECT q,a FROM faq WHERE q="+query)
-  console.log("Q: "+result[0].q)
-  console.log("A: "+result[0].a)
+    usage: "<q>",
+    description: "when in doubt.",
+    // not sure how mart implemented this
+    //but I have to make sure bot prints the file.
+    process: function getEntry(query) {
+        result = faq.run("SELECT q,a FROM faq WHERE q=" + query)
+        console.log("Q: " + result[0].q)
+        console.log("A: " + result[0].a)
+    }
 }
- }
