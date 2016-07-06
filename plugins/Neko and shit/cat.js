b796279c02a0514afa441a523b8eeda9d183dbfa
sr229 <aguilarralph13@gmail.com>
2016-07-06 14:02:47 +0800

    Ovyerus is bae

---
+++ plugins/Neko and shit/cat.js
@@ -0,0 +1,20 @@
+
+exports.commands = [
+    "cat"
+]
+//'tis a bloddy cat generator you cuck.
+var request = require("request");
+exports.cat = {
+    usage : "<desu>",
+    description : "gets a picture of a cat. Seriously, get a real one.",
+    process: 
+    request("http://random.cat/meow/", function(error, response, body){
+
+	    if(!error && response.statusCode == 200){
+
+		var meow = JSON.parse(body);
+
+		bot.sendMessage(msg.channel, meow);
+
+        }
+}
