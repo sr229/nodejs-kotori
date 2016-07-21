df858fc9c5e6308f1fbd20153f1b3258c8d93a33
Capuccino <aguilarralph13@gmail.com>
2016-07-22 00:25:16 +0800

    meh
    Signed-off-by: Capuccino <aguilarralph13@gmail.com>

--- plugins/f/navy_paid_respects_to_ur_ded_cat.js
+++ plugins/f/navy_paid_respects_to_ur_ded_cat.js
@@ -1,10 +1,11 @@
 exports.command = [
-        "f",
-    ]
-    //the most useless command. Made by someone who forgot to sleep for 48 hours.
+    "f",
+],
+//the most useless command. Made by someone who forgot to sleep for 48 hours.
 exports.f {
-    usage: ""
-    description: "pay respects."
-    process: function(bot, msg, suffix)
-    bot.SendMessage(msg.channel, `${msg.author} ,` + "has paid respects.");
+    usage: "",
+    description: "pay respects.",
+    process: function(bot, msg, suffix) {
+        bot.SendMessage(msg.channel, `${msg.author} ,` + "has paid respects.");
+    }
 }
