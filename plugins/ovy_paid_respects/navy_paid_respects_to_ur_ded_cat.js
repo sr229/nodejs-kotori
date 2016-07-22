ac5e9e7ee7cb3a7904286cf46d0f476213224061
Capuccino <aguilarralph13@gmail.com>
2016-07-22 10:09:35 +0800

    [21-gun salute is heard from a distance]
    Signed-off-by: Capuccino <aguilarralph13@gmail.com>

--- plugins/ovy_paid_respects/navy_paid_respects_to_ur_ded_cat.js
+++ plugins/ovy_paid_respects/navy_paid_respects_to_ur_ded_cat.js
@@ -3,7 +3,7 @@ exports.command = [
 ],
 //the most useless command. Made by someone who forgot to sleep for 48 hours.
 exports.f {
-  //wow.such command. much functionality. very NSA. amaze.
+  //wow.such command. much functionality. very navy. amaze.
     description: "pay respects.",
     process: function(bot, msg, suffix) {
         bot.SendMessage(msg.channel, `${msg.author} ,` + "has paid respects.");
