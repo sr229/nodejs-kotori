b7ce473ca594226565a60b28ca1a6768a0bbf85a
Capuccino <aguilarralph13@gmail.com>
2016-07-22 10:22:27 +0800

    meh
    Signed-off-by: Capuccino <aguilarralph13@gmail.com>

--- plugins/Cleverbot/cleverbot.js
+++ plugins/Cleverbot/cleverbot.js
@@ -7,15 +7,18 @@ talkbot = new cleverbot;
 cleverbot.prepare(function() {});
 
 exports.talk = {
-    usage: "<message>",
-    description: "Talk directly to the bot",
-    process: function(bot, msg, suffix) {
-        var conv = suffix.split(" ");
-        talkbot.write(conv, function(response) {
-            bot.sendMessage(msg.channel, `$ {msg.author},`, response.message);
-            if (msg.author != bot.user && msg.isMentioned(bot.user, suffix)) {
-                bot.sendMessage(msg.channel, `$ {msg.author},`, response.message);
+        usage: "<message>",
+        description: "Talk directly to the bot",
+        process: function(bot, msg, suffix) {
+            var conv = suffix.split(" ");
+            talkbot.write(conv, function(response) {
+                    bot.sendMessage(msg.channel, `$ {msg.author},`, response.message);
+                    if (msg.author != bot.user && msg.isMentioned(bot.user, suffix)) {
+                        var conv = suffix.split(" ");
+                        talkbot.write(conv, function(respose) {
+                                bot.sendMessage(msg.channel, `$ {msg.author},`, response.message);
+                            }
+                        }
+                    })
             }
-        })
-    }
-}
+        }
