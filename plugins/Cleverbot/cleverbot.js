7a986999a64a01adb641d45a09c6eb439464882e
Capuccino <aguilarralph13@gmail.com>
2016-07-22 10:15:55 +0800

    nyaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa~!
    Signed-off-by: Capuccino <aguilarralph13@gmail.com>

--- plugins/Cleverbot/cleverbot.js
+++ plugins/Cleverbot/cleverbot.js
@@ -13,7 +13,7 @@ exports.talk = {
         var conv = suffix.split(" ");
         talkbot.write(conv, function(response) {
             bot.sendMessage(msg.channel, `$ {msg.author},`, response.message);
-            if (msg.author != bot.user && msg.isMentioned(bot.user)) {
+            if (msg.author != bot.user && msg.isMentioned(bot.user, suffix)) {
                 bot.sendMessage(msg.channel, `$ {msg.author},`, response.message);
             }
         })
