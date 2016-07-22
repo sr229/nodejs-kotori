443d3557cabeaad58fb723ac9139dcad94d1d233
Capuccino <aguilarralph13@gmail.com>
2016-07-22 10:07:55 +0800

    why am I listening to shitty music
    Signed-off-by: Capuccino <aguilarralph13@gmail.com>

--- plugins/ovy_paid_respects/navy_paid_respects_to_ur_ded_cat.js
+++ plugins/ovy_paid_respects/navy_paid_respects_to_ur_ded_cat.js
@@ -3,7 +3,7 @@ exports.command = [
 ],
 //the most useless command. Made by someone who forgot to sleep for 48 hours.
 exports.f {
-    usage: "",
+  //wow.such command. much functionality. very NSA. amaze.
     description: "pay respects.",
     process: function(bot, msg, suffix) {
         bot.SendMessage(msg.channel, `${msg.author} ,` + "has paid respects.");
