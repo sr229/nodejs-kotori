2f365d90ad1797a794631c2b3a31b656214d1cc1
Capuccino <aguilarralph13@gmail.com>
2016-07-23 11:57:04 +0800

    lewd (#34)
    * lewd
    
    * we don't need this shit
    
    Ovy pls
    
    * nyuuuu
    
    * untrigger gus
    
    wew

--- plugins/Neko and shit/cat.js
+++ plugins/Neko and shit/cat.js
@@ -1,22 +1,50 @@
-
 exports.commands = [
-    "cat",
-    "meow",
-    "nyaa",
-]
-//'tis a bloddy cat generator you cuck.
-var request = require("request");
+        "cat",
+        "meow",
+        "nyaa",
+    ]
+    //'tis a bloddy cat generator you cuck.
 exports.cat = {
-    usage : "<desu>",
-    description : "gets a picture of a cat. Seriously, get a real one.",
-    process: 
-    request("http://random.cat/meow/", function(error, response, body){
+        usage: "",
+        description: "gets a picture of a cat. Seriously, get a real one.",
+        process: function(bot, msg)
+        require("request")("http://random.cat/meow/", function(error, response, body) {
 
-	    if(!error && response.statusCode == 200){
+                if (!error && response.statusCode == 200) {
 
-		var meow = JSON.parse(body);
+                    var meow = JSON.parse(body);
 
-		bot.sendMessage(msg.channel, meow);
+                    bot.sendMessage(msg.channel, parsed.file);
+                } catch (e) {
+                    bot.sendMessage(msg.channel, "oh no! :sob:");
+                }
 
-        }
-    }
+            }
+        },
+        exports.meow {
+            usage: "",
+            description: "gets a picture of a cat.Seriously, get a real one.",
+            process: function(bot, msg)
+            require("request")("http://random.cat/meow", function(error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        var meow = JSON.parse(body);
+                        bot.sendMessage(msg.channel, parsed.file + "n-nya?...nyaaaa~!");
+                    } catch (e) {
+                        bot.sendMessage(msg.channel, "oh no! :sob:");
+                    }
+                }
+            },
+            exports.nyaa {
+                usage: "",
+                description: "gets a picture of a cat.Seriously, get a real one.",
+                process: function(bot, msg) {
+                        require("request")("http://random.cat/meow", function(error, response, body) {
+                                if (!error && response.statuscode == 200) {
+                                    var meow = JSON.parse(body);
+                                    bot.sendMessage(msg.channel, parsed.file + "n-nani!...");
+                                }
+
+                            } catch (e) {
+                                bot.sendMessage(msg.channel, "oh no! :sob:");
+                            }
+                        }
--- plugins/jsonFAQ/faq.js
+++ plugins/jsonFAQ/faq.js
@@ -1,20 +1,37 @@
 exports.commands = [
-    "faq",
+    "faq.json",
     "faq.experm.addentr",
     "faq.experm.delentr",
 
 ];
 //experimental FAQ made using JSON database.
-try{ 
+try {
     faq = require("./faq.json");
-} catch(e){
+} catch (e) {
     //no such file
     faq = {};
 }
 exports.faq.experm.addentr {
-    usage: "<keyword>","<entry>",
-    description: "adds a FAQ entry (experimental, can crash the bot anytime.).",
-    process: function updateFAQ(){
-        require("fs").writefile("./message.json",JSON.stringify(index,description,)null);  
-    }
-}
+        usage: "<keyword>",
+        "<entry>",
+        description: "adds a FAQ entry (experimental, can crash the bot anytime.).",
+        process: function(bot, msg, suffix)
+        var entry = suffix.split(' ');
+        var entry = msg.content.split('');
+        var index = msg.content.split('');
+        var index = args.shift();
+        updateFAQ() {
+            require("fs").writefile("./message.json", JSON.stringify(index, description, ) null);
+            else {
+                    bot.sendMessage(msg.channel, "failed adding entry.");
+            }
+        }
+    },
+    exports.faq.json {
+        process: function(bot, msg, suffix)
+        faq = JSON.parse(fs.readFileSync("./faq.json"));
+        bot.sendMessage(msg.channel, entry);
+        else {
+                bot.sendMessage(msg.channel, "no entry found!");
+            }
+        },
